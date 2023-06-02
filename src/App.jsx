0b2b3655367e3e651647ca49638721f3c0f97325
grandony <trinchvr@gmail.com>
2023-06-02 02:26:16 -0500

    instalando tailwind y material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import { AppRouter } from "./routes/AppRouter"
 
 export const App = () => {
   return (
     <>
-      <h1>Hola mundo</h1>
+      <AppRouter/>
     </>
   )
 }
---
+++ src/page/landinpage/HomePage.jsx
@@ -0,0 +1,6 @@
+
+export const HomePage = () => {
+  return (
+    <div className="bg-red-600">HomePage</div>
+  )
+}
